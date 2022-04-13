67a8b447f4be6c2dbb68514a3e1ea74d838dae4c
harikae86 <harmath.esztike@gmail.com>
2022-04-13 15:27:44 +0200

    add pagination

---
+++ src/components/Pagination.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-col cols="12" class="d-flex justify-center my-2 pt-0">
+    <v-btn
+      class="mx-2 p-2"
+      fab
+      dark
+      medium
+      color="accent"
+      v-on:click.prevent="previous()"
+    >
+      <v-icon> fa-solid fa-arrow-left </v-icon>
+    </v-btn>
+    <v-btn
+      class="mx-2"
+      fab
+      dark
+      medium
+      color="accent"
+      v-on:click.prevent="next()"
+    >
+      <v-icon dark> fa-solid fa-arrow-right /> </v-icon>
+    </v-btn>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: "Pagination-comp",
+  data() {
+    return {
+      page: 1,
+    };
+  },
+  methods: {
+    next() {
+      this.$emit("nextPage", this.page);
+    },
+    previous() {
+      this.$emit("prevPage", this.page);
+    },
+  },
+};
+</script>
