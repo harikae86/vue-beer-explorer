c32fce6e65a3b6a8c1ed26817280dfc0ad911934
harikae86 <harmath.esztike@gmail.com>
2022-04-13 15:27:33 +0200

    add randombeer feature

---
+++ src/views/RandomBeerView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <BeerCard :beers="this.beers" />
+  </div>
+</template>
+
+<script>
+import BeerCard from "../components/BeerCard.vue";
+export default {
+  name: "Random-beer",
+  components: { BeerCard },
+  data() {
+    return {
+      beers: [],
+    };
+  },
+  mounted() {
+    this.getRandomBeer();
+  },
+  methods: {
+    async getRandomBeer() {
+      const url = "https://api.punkapi.com/v2/beers/random";
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        this.beers = data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
